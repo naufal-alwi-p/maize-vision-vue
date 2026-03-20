d77474162497d6c26522483272f0052c7d8f5e5a
naufal-alwi-p <naufalpratama992@gmail.com>
2026-03-20 15:01:03 +0700

    feat: add PrimeVue components and integrate Tailwind CSS with PrimeUI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Menubar from 'primevue/menubar'
+import { ref } from "vue";
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        badge: 3,
+        items: [
+            {
+                label: 'Core',
+                icon: 'pi pi-bolt',
+                shortcut: '⌘+S'
+            },
+            {
+                label: 'Blocks',
+                icon: 'pi pi-server',
+                shortcut: '⌘+B'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'UI Kit',
+                icon: 'pi pi-pencil',
+                shortcut: '⌘+U'
+            }
+        ]
+    }
+]);
 </script>
 
 <template>
+  <div>
+    <Menubar :model="items" />
+  </div>
+
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import Button from "primevue/button"
+</script>
+
 <template>
   <main>
     <h1>Halo</h1>
+
+    <Button severity="info" @click="console.log('Halo')">Click</Button>
 
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
