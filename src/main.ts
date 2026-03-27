9076c4b8ebe23b947a10248c01116bceba84d3d3
naufal-alwi-p <naufalpratama992@gmail.com>
2026-03-27 21:19:38 +0700

    Install primeicons library *notes: also fix npm audit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from "@primeuix/themes/aura"
 
+import 'primeicons/primeicons.css'
+
 const app = createApp(App)
 
 app.use(router)
