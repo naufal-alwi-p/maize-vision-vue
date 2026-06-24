76d0daf9f4ddca81ce07ae092381674bfb0f84b2
naufal-alwi-p <naufalpratama992@gmail.com>
2026-06-24 16:24:34 +0700

    feat: add custom scroll behavior to router for improved navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,13 @@ import DiseaseView from '../views/DiseaseView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
   sensitive: true,
   routes: [
     {
