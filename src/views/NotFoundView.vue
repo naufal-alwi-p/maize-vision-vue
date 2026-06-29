ad939e682814baf0adff0208492abf086cc17ca9
naufal-alwi-p <naufalpratama992@gmail.com>
2026-06-30 01:26:51 +0700

    feat: add NotFound route and component for handling 404 errors

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="min-h-[calc(100vh-4rem)] bg-[#faf8f1] flex flex-col items-center justify-center gap-4">
+        <p class="text-3xl text-gray-500 font-bold">404</p>
+        <h1 class="text-7xl font-bold">Not Found</h1>
+        <RouterLink to="/" class="text-blue-600 hover:text-blue-800 underline">
+            Go Home
+        </RouterLink>
+    </div>
+</template>
